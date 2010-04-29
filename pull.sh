b4591d91e8fd0436065b94e59a9857345b529bd0
Nick Markwell <nick@duckinator.net>
2010-04-28 22:36:44 -0400

    Added pull.sh (for automatically pulling the repo)

---
+++ pull.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# To generate the cron entry, run this script with -cron
+
+SCRIPT=$(readlink -f $0)
+DIR=`dirname $SCRIPT`
+LOG=$DIR/git_pull.log
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" != "" ]; then
+  echo
+  echo "Add the following cron entry for this to auto-run every 10 minutes:"
+  echo
+  echo "*/10 * * * * ${SCRIPT}"
+  echo
+  exit
+fi
+
+cd $DIR
+echo -en "$(date):\n  " >> $LOG
+git pull &>> $LOG
+
