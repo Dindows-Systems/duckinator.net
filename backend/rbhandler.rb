894f7414253395c6ee943326e6fd6ee8365e815f
Nick Markwell <nick@duckinator.net>
2010-05-09 03:20:16 -0400

    Tweak

--- backend/rbhandler.rb
+++ backend/rbhandler.rb
@@ -1,6 +1,14 @@
 require 'rubygems'
 require 'cgi'
 
-cgi = CGI.new
-cgi.out { `/usr/bin/env ruby #{ENV['PATH_TRANSLATED']}` }
+def run
+	`/usr/bin/env ruby #{ENV['PATH_TRANSLATED']}`
+end
+
+if open(ENV['PATH_TRANSLATED']).read[0...10] == "#noheaders")
+	run
+else
+	cgi = CGI.new
+	cgi.out { run }
+end
 
