ac321c6bf411acf22eba48fe74e4afe48936a44f
Nick Markwell <nick@duckinator.net>
2010-05-09 03:26:54 -0400

    Roar, frozen strings :(

--- public/themes/preview/index.rb
+++ public/themes/preview/index.rb
@@ -1,7 +1,10 @@
 #noheaders
 require File.join(File.dirname(__FILE__), '..', '..', '..', 'backend', 'backend.rb')
 
-env = ENV.clone
+env = {}
+ENV.each do |k,v|
+	env[k]=v
+end
 env['PATH_TRANSLATED'].delete!('themes/preview/')
 env['PATH_TRANSLATED'].gsub!('/index.rb', '/index.rhtml')
 
