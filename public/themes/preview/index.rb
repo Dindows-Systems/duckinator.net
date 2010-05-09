ce942c4c9cfa61fa294f5302b9b9a267612cd447
Nick Markwell <nick@duckinator.net>
2010-05-09 03:25:04 -0400

    Hmmm

--- public/themes/preview/index.rb
+++ public/themes/preview/index.rb
@@ -2,7 +2,8 @@
 require File.join(File.dirname(__FILE__), '..', '..', '..', 'backend', 'backend.rb')
 
 env = ENV.clone
-env['PATH_TRANSLATED'] = env['PATH_TRANSLATED'].delete('themes/preview/').gsub('/index.rb', '/index.rhtml')
+env['PATH_TRANSLATED'].delete!('themes/preview/')
+env['PATH_TRANSLATED'].gsub!('/index.rb', '/index.rhtml')
 
 HomePage.new(env, 'dove')
 
