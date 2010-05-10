d57258a88be39d0a81e2c026100cfa0c30be3272
Nick Markwell <nick@duckinator.net>
2010-05-09 22:21:41 -0400

    Weeeeeeeeeeeeeeeeeee

--- backend/backend.rb
+++ backend/backend.rb
@@ -69,7 +69,8 @@ class HomePage
 
     text = parse_liquid(text)
     if !@location_override.nil?
-      text.gsub!(/<a href="\//, "<a href=\"#{@location_override}/")
+      text.gsub!(/<a href='\//, "<a href='#{@location_override}/")
+      text.gsub!(/<a href="\//, "<a href="#{@location_override}/")
     end
     text
   end
