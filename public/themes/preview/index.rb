04997c44afdbaabcd64bbb87ba4e7213e0eaffd6
Nick Markwell <nick@duckinator.net>
2010-05-09 21:21:24 -0400

    I haven't the slightest idea if this will work :D

--- public/themes/preview/index.rb
+++ public/themes/preview/index.rb
@@ -4,7 +4,16 @@ require 'cgi'
 
 env = ENV.clone
 
-env['PATH_TRANSLATED'] = ENV['PATH_TRANSLATED'].gsub('themes/preview/', '').gsub('/index.rb', '/index.rhtml')
+def parse_query_string(query)
+	parts = query.split(/&|=/)
+	Hash[*parts]
+end
+
+dir = env['PATH_TRANSLATED'].gsub('themes/preview/index.rb','')
+query_string = env['REDIRECT_QUERY_STRING']
+query_hash = parse_query_string(query_string)
+env['PATH_TRANSLATED'] = File.join(dir, query_hash['path'])
+#env['PATH_TRANSLATED'] = ENV['PATH_TRANSLATED'].gsub('themes/preview/', '').gsub('/index.rb', '/index.rhtml')
 
 cgi = CGI.new
 cgi.out {
