64dee1c90e0ff3dc34bfb7696f2bce3d83c33400
Nick Markwell <nick@duckinator.net>
2010-05-10 22:30:54 -0400

    Random )'s are evil

--- backend/backend.rb
+++ backend/backend.rb
@@ -41,7 +41,7 @@ class HomePage
       i = location_words.length-1
       i-=1 until title_check(location_words, i) || i == 0
       if i > 1
-        @title = "#{location_words[i]} : #{location_words[0...i)].join(' ')} : #{@title}"
+        @title = "#{location_words[i]} : #{location_words[0...i].join(' ')} : #{@title}"
         (0...i).each do |n|
           link = "/" + location_words[0...n].join('/')
           @breadcrumbs = "#{generate_link(link, location_words[n])} : #{breadcrumbs}"
