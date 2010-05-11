ffc7e5bacd7a84d78048ac8e7afe4f5dbe85926f
Nick Markwell <nick@duckinator.net>
2010-05-10 21:01:29 -0400

    Added link to logs. Now, to play UrT!

--- public/autopull/index.rb
+++ public/autopull/index.rb
@@ -1,5 +1,5 @@
 require 'cgi'
 cgi = CGI.new
 cgi.out {
-	"Pulling from github... #{`git pull &> autopull/log`}Done."
+	"Pulling from github... #{`git pull &> autopull/log`}Done. <a href=\"log\">log</a>"
 }
