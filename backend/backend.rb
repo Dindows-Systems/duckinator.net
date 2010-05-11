11842eed323a7405f5d66074335858d1787aceac
Nick Markwell <nick@duckinator.net>
2010-05-10 22:52:59 -0400

    `if i > 1` -> `if i >= 1`

--- backend/backend.rb
+++ backend/backend.rb
@@ -40,7 +40,7 @@ class HomePage
 
       i = location_words.length-1
       i-=1 until title_check(location_words, i) || i == 0
-      if i > 1
+      if i >= 1
         (1..i).each do |n|
           link = location_words[0..n].join('/')
           @breadcrumbs = "#{generate_link(link, location_words[n])} : #{@breadcrumbs}"
