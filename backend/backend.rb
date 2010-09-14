e8618a51d07938240230a066d8202b4bf4e17f3a
Nick Markwell <nick@duckinator.net>
2010-09-14 14:43:33 -0400

    Well, that last one didn't work... :D

--- backend/backend.rb
+++ backend/backend.rb
@@ -19,8 +19,8 @@ class HomePage
     file = "#{env['DOCUMENT_ROOT']}#{env['PATH_INFO']}"
     if file[0..8] == "/preview/" && file.length > 9
       theme, filename = file[9..-1].split("/", 2)
-      filename = "/" + filename
-      Homepage.new(env, theme, filename)
+      env['PATH_INFO'] = filename
+      Homepage.new(env, theme)
     else
       if File.directory?(file)
         file = "#{file}/index.md"
