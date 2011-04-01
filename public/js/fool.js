74d3d6d0b15c4cee589b173a5ba55a8446d1090c
Nick Markwell <nick@duckinator.net>
2011-04-01 16:51:22 -0400

    My attempt at being smart backfired D;

--- public/js/fool.js
+++ public/js/fool.js
@@ -1,7 +1,7 @@
 // Based on code at http://thechangelog.com/post/4258932732/fool-js-april-1st-javascript-snippet
 // This script turns the page sideways if it's April 1st
 var date = new Date();
-if ([date.getMonth(), date.getDate()] == [3, 1]) { // If it's April 1
+if (date.getMonth() == 3 && date.getDate() == 1) { // If it's April 1
   //  Uncomment the next line if you want the prank to happen only when the url hashtag is #april
   if (document.location.hash.indexOf('april') > -1) {
     document.body.style.transform =
