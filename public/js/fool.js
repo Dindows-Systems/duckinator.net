92fddbe8f1bc1c996f6045912d13793bf476f8aa
Nick Markwell <nick@duckinator.net>
2011-04-01 17:00:37 -0400

    Escape link

--- public/js/fool.js
+++ public/js/fool.js
@@ -1,5 +1,17 @@
 // Based on code at http://thechangelog.com/post/4258932732/fool-js-april-1st-javascript-snippet
 // This script turns the page sideways if it's April 1st
+
+function AprilFoolsCreateEscape() {
+  div = document.createElement("div");
+  div.setAttribute('position', 'absolute');
+  div.setAttribute('top', '0px');
+  div.setAttribute('left', '0px');
+  
+  a = document.createElement("a");
+  a.innerText = "Help everything is spinning and I'm scared!!!!!!!";
+  a.setAttribute('href', '#help-everything-is-spinning-and-im-scared');
+}
+
 function AprilFools() {
   if(!document.body) {
     setTimeout("AprilFools();", 100);
@@ -9,7 +21,8 @@ function AprilFools() {
   var date = new Date();
   if (date.getMonth() == 3 && date.getDate() == 1) { // If it's April 1
     //  Uncomment the next line if you want the prank to happen only when the url hashtag is #april
-    if (document.location.hash.indexOf('april') > -1) {
+    if (document.location.hash.indexOf('help-everything-is-spinning-and-i-am-scared') < 0) {
+      AprilFoolsCreateEscape();
       document.body.style.transform =
       document.body.style.webkitTransform =
       document.body.style.MozTransform =
