bcdf5093eaaade2b5a3456c85802fa0c3d8db3e8
Nick Markwell <nick@duckinator.net>
2011-04-01 16:54:52 -0400

    <3 timeouts

--- public/js/fool.js
+++ public/js/fool.js
@@ -1,14 +1,23 @@
 // Based on code at http://thechangelog.com/post/4258932732/fool-js-april-1st-javascript-snippet
 // This script turns the page sideways if it's April 1st
-var date = new Date();
-if (date.getMonth() == 3 && date.getDate() == 1) { // If it's April 1
-  //  Uncomment the next line if you want the prank to happen only when the url hashtag is #april
-  if (document.location.hash.indexOf('april') > -1) {
-    document.body.style.transform =
-    document.body.style.webkitTransform =
-    document.body.style.MozTransform =
-    document.body.style.OTransform =
-    document.body.style.msTransform =
-    'rotate(' + [45, 90, 135, 180, 225, 270, 315][Math.floor(Math.random() * 7)] + 'deg)';
+function AprilFools() {
+  if(!document.body) {
+    setTimeout("AprilFools();", 100);
+    return;
+  }
+  
+  var date = new Date();
+  if (date.getMonth() == 3 && date.getDate() == 1) { // If it's April 1
+    //  Uncomment the next line if you want the prank to happen only when the url hashtag is #april
+    if (document.location.hash.indexOf('april') > -1) {
+      document.body.style.transform =
+      document.body.style.webkitTransform =
+      document.body.style.MozTransform =
+      document.body.style.OTransform =
+      document.body.style.msTransform =
+      'rotate(' + [45, 90, 135, 180, 225, 270, 315][Math.floor(Math.random() * 7)] + 'deg)';
+    }
   }
 }
+
+AprilFools();
