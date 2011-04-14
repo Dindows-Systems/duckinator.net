413298468885bdaaa9d29594c28141bd4d7fcdc6
Nick Markwell <nick@duckinator.net>
2011-04-14 02:06:41 -0400

    Added /stalker...errr...I mean... /contact

---
+++ public/contact/index.rb
@@ -0,0 +1,12 @@
+$body = <<EOF
+## Contact ##
+
+Why hello there, my friendly stalker buddy. Feel free to sneak around behind me in a dark alley via the following:
+
+* [Github](http://github.com/duckinator)
+* [BitBucket](http://bitbucket.org/duckinator)
+* [profile.io](http://profile.io/duckinator)
+* [LinkedIn](http://www.linkedin.com/pub/nick-markwell/20/261/325)
+* [Twitter](http://twitter.com/duckinator)
+EOF
+
--- public/profiles/index.rb
+++
@@ -1,4 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', '..', 'backend', 'backend.rb')
-
-$body = "Shazam"
-
