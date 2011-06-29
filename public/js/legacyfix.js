58486774cfa09479dd7cce44a50f6130daba4360
Nick Markwell <nick@duckinator.net>
2011-06-29 01:35:16 -0400

    Added legacy fix for mozilla browsers

---
+++ public/js/legacyfix.js
@@ -0,0 +1,17 @@
+function _legacy_moz_fix_tag(tag) {
+	tags = document.getElementsByTagName(tag);
+	if(tags[0] && (tags[0].getAttribute('_moz-userdefined') != null)) {
+		for(var i in tags) {
+			tags[i].style.display='none';
+		}
+	}	
+}
+
+function legacyfix() {
+	moz_tags = ['header', 'article'];
+	for(var i in moz_tags) {
+		_legacy_moz_fix_tag(moz_tags[i]);
+	}
+}
+
+legacyfix();
