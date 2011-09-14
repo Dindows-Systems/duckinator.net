b366a90a61bbea6121174d923cd194c024171d09
Nick Markwell <nick@duckinator.net>
2011-09-14 17:38:26 -0400

    This should completely fix render.rb, now...

--- render/render.rb
+++ render/render.rb
@@ -17,8 +17,8 @@ class Renderer
     @config  = config[@appname]
 
     Liquid::Template.register_filter(TextFilter)
-    @old_root = File.join(File.dirname(__FILE__), '..', 'public')
-    @new_root = File.join(File.dirname(__FILE__), '..', 'static')
+    @old_root = File.expand_path(File.join(File.dirname(__FILE__), '..', 'public'))
+    @new_root = File.expand_path(File.join(File.dirname(__FILE__), '..', 'static'))
 
     @template = File.open(File.join(File.dirname(__FILE__), '..', 'template.html')) do |f|
       f.read
@@ -56,7 +56,7 @@ class Renderer
       if File.directory?(f)
         render(f)
       elsif File.file?(f)
-        copy_file(f)
+        save_file(f)
       end
     end
     puts "Leaving #{pretty_dir}..."
@@ -65,26 +65,28 @@ class Renderer
   def copy_file(file, raw = true)
     text = File.open(file, 'rb') {|f| f.read }
     
-    old_file = File.join(File.expand_path(File.dirname(file)), File.basename(file))
-    new_file = file.gsub(@old_root, @new_root)
-    save_file(new_file, text)
+    save_file(file, text)
   end
 
-  def save_file(file, text)
+  def save_file(file)
     old_file = File.join(File.expand_path(File.dirname(file)), File.basename(file))
+    new_dir = File.dirname(old_file).sub(@old_root, @new_root)
     new_file = old_file.sub(@old_root, @new_root)
+    
+    text = File.open(file, 'rb') {|f| f.read }
 
     if markdown?(file) || inc?(file)
-      text = generate_html(file)
+      text = parse_file(file, true)
       new_file.sub!(/\.(md|markdown|inc)$/, '.html')
+    elsif css?(file) || javascript?(file)
+      text = parse_file(file)
     else
       text = File.open(old_file, 'rb') {|f| f.read }
     end
     
-    new_dir = File.dirname(new_file).sub(@old_root, @new_root)
     FileUtils.mkdir_p(new_dir)
-    
-    File.open(file, 'wb') {|f| f.print content }
+
+    File.open(new_file, 'wb') {|f| f.print text }
   end
 
   def parse_liquid(text, opts)
@@ -94,7 +96,7 @@ class Renderer
 
   def parse_file(file, html = false)
     content = File.open(file) {|f| f.read }
-    breadcrumbs = BreadCrumbs.new(filename)
+    breadcrumbs = BreadCrumbs.new(file)
     opts = {
             'breadcrumbs' => breadcrumbs,
             'title'       => @config['environment']['title'].value 
