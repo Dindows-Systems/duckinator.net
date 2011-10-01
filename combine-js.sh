d96a8616e9b4a66effeac77be53e557ebff01ffb
Nick Markwell <nick@duckinator.net>
2011-10-01 11:22:09 -0400

    Make combine-js.sh not do anything if static/js doesn't exist

--- combine-js.sh
+++ combine-js.sh
@@ -2,6 +2,11 @@
 
 DIR=$(dirname $(readlink -f $0)) # Directory script is in
 
+if [ ! -d "$DIR/static/js" ]; then
+    echo "Please run $0 after rendering html"
+    exit 1
+fi
+
 cd $DIR/static/js
 
 echo > all.js
