3b2546b24d568548bad94319d8852df09439a8bd
Nick Markwell <nick@duckinator.net>
2012-07-06 17:53:49 -0400

    Minify js/all.js

--- _concatenate_js.sh
+++ _concatenate_js.sh
@@ -4,13 +4,11 @@ DIR=$(dirname $(readlink -f $0)) # Directory script is in
 
 cd $DIR/js
 
-echo > all.js
+echo -n > all.js
 
 for x in *.js; do
   [ "$x" == "all.js" ] && continue
-  echo "/* start $x */" >> all.js
-  cat $x >> all.js
-  echo >> all.js
-  echo "/* end   $x */" >> all.js
+  echo "Comrpessing $x, appending to all.js..."
+  yuicompressor --type js $x >> all.js
   echo >> all.js
 done
