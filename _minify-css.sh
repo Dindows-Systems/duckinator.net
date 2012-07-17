a4a51a05dfc08d125d2650dbba3e5533203d5b61
Nick Markwell <nick@duckinator.net>
2012-07-17 01:16:58 -0400

    Whoops.

--- _minify-css.sh
+++ _minify-css.sh
@@ -9,7 +9,8 @@ echo -n > all.min.css
 for x in *.css; do
   [ "$x" == "all.min.css" ] && continue
   # TODO: Figure out how to make csstidy be niec.
-  csstidy $x --template=low $x.tmp
+  #csstidy $x --template=low $x.tmp
+  cat $x > $x.tmp
 done
 
 for x in *.tmp; do
