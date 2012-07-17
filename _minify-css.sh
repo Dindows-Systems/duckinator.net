3164e4d5aabdf0c35ddb57847c27dc097fc6b491
Nick Markwell <nick@duckinator.net>
2012-07-17 01:15:37 -0400

    Apply some amount of minification...

--- _minify-css.sh
+++ _minify-css.sh
@@ -9,8 +9,7 @@ echo -n > all.min.css
 for x in *.css; do
   [ "$x" == "all.min.css" ] && continue
   # TODO: Figure out how to make csstidy be niec.
-  #csstidy $x --template=highest $x.tmp
-  cat $x > $x.tmp
+  csstidy $x --template=low $x.tmp
 done
 
 for x in *.tmp; do
