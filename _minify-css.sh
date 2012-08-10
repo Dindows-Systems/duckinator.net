1027ad94a7800c2cbbfbe5eb24ee697a8aeff12d
Nick Markwell <nick@duckinator.net>
2012-08-10 01:26:20 -0400

    My home-grown CSS minifier actually works. Unlike the other's I've tried...

--- _minify-css.sh
+++ _minify-css.sh
@@ -9,8 +9,28 @@ echo -n > all.min.css
 for x in {normalize,main}.css; do
   [ "$x" == "all.min.css" ] && continue
   # TODO: Figure out how to make csstidy be niec.
-  #csstidy $x --template=low $x.tmp
+  #csstidy $x --template=default $x.tmp
   cat $x > $x.tmp
+
+  sed -i  's|/\*.*\*/||g'             $x.tmp
+  sed -i  's/\t\|^[\t ]*//g'          $x.tmp
+  sed -i  's/ *\(:\|{\|}\) */\1/g'    $x.tmp
+  #sed -i 's/,\n/,/g'                 $x.tmp
+
+  # foo,\nbar -> foo,bar
+  sed -i -n '1h;1!H;${;g;s/,\n/,/g;p;}' $x.tmp
+
+  # foo\n} -> foo}
+  sed -i -n '1h;1!H;${;g;s/\n}/}/g;p;}' $x.tmp
+
+  # We run this 3 times because I want to avoid loops.
+  # \n\n -> \n
+  sed -i -n '1h;1!H;${;g;s/\n\n/\n/g;p;}' $x.tmp
+  sed -i -n '1h;1!H;${;g;s/\n\n/\n/g;p;}' $x.tmp
+  sed -i -n '1h;1!H;${;g;s/\n\n/\n/g;p;}' $x.tmp
+
+  # Remove comments
+  #sed -i -n '1h;1!H;${;g;s|/\*(^\*/)+\*/||g;p;}' $x.tmp
 done
 
 for x in {normalize,main}.css.tmp; do
